b1b84a995e4454005623704acb77446cc3fddd06
Richard Whitehead <damian253@me.com>
2020-01-15 21:09:11 -0800

    inital sacafold

--- js/app.js
+++ js/app.js
@@ -1,3 +1,64 @@
 'use strict';
 
-prompt('What is your name?');
+console.log('Quiz');
+
+var enterName = prompt('Hello! What is your name?');
+
+var userWantsToReadBio = prompt('Hi ' + enterName + ', Ready to read Bio and take a short quiz?');
+
+if (userWantsToReadBio.toLowerCase() === 'yes') {
+  alert('Awesome, Lets Start');
+} else if (userWantsToReadBio === 'no') {
+  alert('Please exit the Bio');
+} else {
+  alert('Something went wrong! Try again');
+}
+
+var questionOne = prompt('Did Richard join the Army in 1997?');
+
+if (questionOne.toLowerCase() === 'yes') {
+  alert('That is correct!');
+} else if (questionOne === 'no') {
+  alert('Incorrect! Try again');
+} else {
+  alert('Please try again!');
+}
+
+var questionTwo = prompt('Do Richard have three daughters?');
+
+if (questionTwo.toLowerCase() === 'yes') {
+  alert('That is correct!');
+} else if (questionTwo === 'no') {
+  alert('Incorrect! Try again');
+} else {
+  alert('Please try again!');
+}
+
+var questionThree = prompt('Is Richard married?');
+
+if (questionThree.toLowerCase() === 'yes') {
+  alert('That is correct!');
+} else if (questionThree === 'no') {
+  alert('Incorrect! Try again');
+} else {
+  alert('Please try again!');
+}
+var questionFour = prompt('Does Richard only have one son?');
+
+if (questionFour.toLowerCase() === 'yes') {
+  alert('That is correct!');
+} else if (questionFour === 'no') {
+  alert('Incorrect! Try again');
+} else {
+  alert('Please try again!');
+}
+var questionFive = prompt('Was Richard commissioned as a Officer?');
+
+if (questionFive.toLowerCase() === 'yes') {
+  alert('That is correct!');
+} else if (questionFive === 'no') {
+  alert('Incorrect! Try again');
+} else {
+  alert('Please try again!');
+}
+
